cbf2014f372db332cf01444ce1db6a1870a2ba66
Javier Godinez <godinezj@gmail.com>
2024-01-12 11:41:41 -0800

    initial infra

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "bastion" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+  key_name      = "javier"
+  subnet_id     = "subnet-a9d32fc2"
+  tags = {
+    Name = "Bastion"
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+  key_name      = "javier"
+  subnet_id     = "subnet-00c81127c4cf2ac81"
+  tags = {
+    Name = "appserver"
+  }
+}
+
